52b66dff2c148a2267819b0fc2c9899d0f1baa79
zchirah <156684307+poyoro@users.noreply.github.com>
2024-06-23 13:19:09 +0800

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,3 +1,16 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('nav-toggle');
+});
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('nav-toggle');
+}
+
 function displayModal() {
     console.log('displayModal called');
     const modal = document.getElementById('weather-clothing-modal');
